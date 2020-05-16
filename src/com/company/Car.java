3c62f823cad1e8af534e18dd4ec3070cf71ad2aa
Jan Dembowski <65111622+jasiudembowski@users.noreply.github.com>
2020-05-16 14:24:33 +0200

    Add new files

---
+++ src/com/company/Car.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Car {
+    String producer;
+    String model;
+    Integer year;
+    public Car(String  producer, String model, Integer year) {
+        this.model = model;
+        this.producer = producer;
+        this.year = year;
+    }
+
+
+}
+
